b94166ba0a4b5e17a9f4b1e283a2fc51ceb07af7
Albion Krasniqi <55230142+AlbionKransiqi@users.noreply.github.com>
2022-04-21 20:49:23 -0700

    data pre-processing

--- data_pre-processing.py
+++ data_pre-processing.py
@@ -1,9 +1,9 @@
-import warnings
+# importinh necessary libraries
 from datetime import datetime
 import numpy as np
-warnings.filterwarnings('ignore')
 import pandas as pd
-olympics=pd.read_csv("Downloads/ss154_data_collection/data/olympic_hosts.csv")
+
+olympics=pd.read_csv("Downloads/data/olympic_hosts.csv")
 
 #dropping the unnecessary rows
 olympics.drop(olympics[olympics.Type=='youthgames'].index, inplace=True)
@@ -12,10 +12,9 @@ olympics.drop(olympics[olympics.Year<2002].index, inplace=True)
 
 #dropping those treated more than once
 olympics = olympics.drop_duplicates(subset=['Country'], keep=False)
-
 countries = olympics['Country'].to_numpy()
 
-#generating ids
+#generating ids for countries
 def generate_id(country):
     return list(countries).index(country)+1
 
@@ -28,6 +27,7 @@ olympics['Subtype'] = pd.get_dummies(olympics['Type'])
 
 olympics['Date'] = [str(i)+' '+j for i, j in zip(olympics.Year.to_numpy(), olympics.Date.to_numpy())]
 
+# converting date
 def date_filter(date):
     new = date.split("-")[0]
     new = new + "10:00:00"
@@ -41,38 +41,21 @@ olympics = olympics[['Country', 'Year', 'id', 'Subtype', 'Post', 'Days']]
 #adding the panel data for different years
 years = np.arange(min(olympics['Year'].unique()), max(olympics['Year'].unique())+1, 1)
 
-for country in countries:
-    host_year = olympics[olympics.Country==country].Year.to_numpy()
-    host_id = olympics[olympics.Country==country].id.to_numpy()[0]
-    host_type = olympics[olympics.Country==country].Subtype.to_numpy()[0]
-
-    for year in years:
-        if host_year > year:
-            olympics = olympics.append({'Country': country, 'Year': year, 'id': host_id, 'Subtype': host_type, 'Post': 0}, ignore_index=True)
-
-        elif host_year < year:
-            olympics = olympics.append({'Country': country, 'Year': year, 'id': host_id, 'Subtype': host_type, 'Post': 1}, ignore_index=True)
-
-        else:
-            idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
-            if olympics['Subtype'][idx].to_numpy()[0] == 1:
-                olympics['Post'][idx] = (366-olympics['Days'][idx].to_numpy()[0])/366
-            else:
-                olympics['Post'][idx] = (365-olympics['Days'][idx].to_numpy()[0])/365
-            
+		
 #adding the needed variables 
-gdpc = pd.read_csv('Downloads/ss154_data_collection/data/gdpc.csv')
+gdpc = pd.read_csv('Downloads/data/gdpc.csv')
 olympics['gdpc'] = [0 for i in range(len(olympics))]
 
-fin = pd.read_csv('Downloads/ss154_data_collection/data/fin_data.csv')
+fin = pd.read_csv('Downloads/data/fin_data.csv')
 olympics['FinStability'] = [0 for i in range(len(olympics))]
 
-freedom = pd.read_csv('Downloads/ss154_data_collection/data/freedom.csv')
+freedom = pd.read_csv('Downloads/data/freedom.csv')
 olympics['Freedom'] = [0 for i in range(len(olympics))]
 
-polit = pd.read_csv('Downloads/ss154_data_collection/data/polit.csv')
+polit = pd.read_csv('Downloads/data/polit.csv')
 olympics['PolitStability'] = [0 for i in range(len(olympics))]
 
+# for each country and year adding the other variables
 for country in countries:
     for year in years:
         idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
@@ -93,19 +76,6 @@ for country in countries:
         idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
         olympics['PolitStability'][idx] = polit[(polit['Country Name']==country)&(polit['Series Name']=='Political Stability and Absence of Violence/Terrorism: Estimate')]['{0} [YR{0}]'.format(year)].to_numpy()[0]
         
-#adding the lags and leads
-diff = []
-olympics['Diff']= [0 for i in range(len(olympics))]
-indices = olympics.index.to_numpy()
-for idx in indices:
-    host_year = olympics[(olympics.Country == olympics.iloc[idx].Country) & (olympics.Post<1)].Year.to_numpy()[0]
-    olympics['Diff'][idx]=olympics.iloc[idx].Year - host_year
-
-#adding dummy
-one_hot = pd.get_dummies(olympics[olympics.Diff<0].Diff, prefix='lead')
-two_hot = pd.get_dummies(olympics[olympics.Diff>0].Diff, prefix='lag')
-olympics = pd.concat([olympics, one_hot, two_hot], axis=1)
-olympics = olympics.fillna(0)
 
 olympics = olympics.sort_values(by=['id', 'Year'])
 
@@ -126,11 +96,4 @@ olympics = olympics.sort_values(by=['id', 'Year'])
 ------------------------------------------------------
 
 # saving the data 
-olympics.to_csv("Downloads/ss154_data_collection/olympics.csv")
-
-olympics_winter = olympics[olympics.Subtype==0]
-olympics_summer = olympics[olympics.Subtype==1]
-
-olympics_winter.to_csv("Downloads/ss154_data_collection/olympics_winter.csv")
-olympics_summer.to_csv("Downloads/ss154_data_collection/olympics_summer.csv")
-
+olympics.to_csv("Downloads/olympics.csv")
